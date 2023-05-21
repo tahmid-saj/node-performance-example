89466e74a1ea61acbfe2cd8154a21a65d984b52c
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-21 04:28:33 -0400

    Common slow causes

--- server.js
+++ server.js
@@ -11,12 +11,17 @@ function delay(duration) {
 }
 
 app.get("/", (req, res) => {
+    // JSON.stringify({} => "{}");
+    // JSON.parse("{}") => {};
+    // [1, 2, 3, 4, 0].sort()
+
     res.send("Performance example");
 });
 
 app.get("/timer", (req, res) => {
     // Delay the response
     delay(9000);
+
     res.send("Ding ding ding");
 });
 
