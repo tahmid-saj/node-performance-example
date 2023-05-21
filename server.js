47da378f9883db6e8c3d54ce0de6b9120bc94554
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-21 04:55:49 -0400

    Adding additional processes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cluster = require("cluster");
+const os = require("os");
 
 const app = express();
 
@@ -15,14 +17,25 @@ app.get("/", (req, res) => {
     // JSON.parse("{}") => {};
     // [1, 2, 3, 4, 0].sort()
 
-    res.send("Performance example");
+    res.send(`Performance example ${process.pid}`);
 });
 
 app.get("/timer", (req, res) => {
     // Delay the response
     delay(9000);
 
-    res.send("Ding ding ding");
+    res.send(`Ding ding ding ${process.pid}`);
 });
 
-app.listen(3000);
+console.log("Running server.js");
+if (cluster.isMaster) {
+    console.log("Master has been started...");
+    const NUM_WORKERS = os.cpus().length;
+    for (let i = 0; i < NUM_WORKERS; i++) {
+        cluster.fork();
+    }
+
+} else {
+    console.log("Worker process started");
+    app.listen(3000);
+}
