260ad60c7880dff0d3ffa229e41d62de1e2dac82
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-21 05:16:47 -0400

    Adding pm2

--- server.js
+++ server.js
@@ -28,14 +28,5 @@ app.get("/timer", (req, res) => {
 });
 
 console.log("Running server.js");
-if (cluster.isMaster) {
-    console.log("Master has been started...");
-    const NUM_WORKERS = os.cpus().length;
-    for (let i = 0; i < NUM_WORKERS; i++) {
-        cluster.fork();
-    }
-
-} else {
-    console.log("Worker process started");
-    app.listen(3000);
-}
+console.log("Master has been started...");
+app.listen(3000);
